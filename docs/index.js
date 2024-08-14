016e8343e6c131c51ae454e84ef56bcd169ddb32
JD <izukanigawa@gmail.com>
2024-08-14 21:19:21 +0800

    updated

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,8 @@ var userName = document.forms['form']['userName'];
 var name_error = document.getElementById('name_error');
 
 function validation() {
-    if (userName.value !== 'Krystel') {
-        name_error.textContent = 'Invalid name. Lagay mo yung name mo hahahah.';
+    if (userName.value !== "Krystel") {
+        name_error.textContent = "Invalid name. Lagay mo yung name mo hahahah.";
         name_error.style.color = 'red';
         return false; // Prevent form submission
     } else {
