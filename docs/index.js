1a83567cb62a04555a3576a40224f6675f35d264
JD <izukanigawa@gmail.com>
2024-08-14 22:16:56 +0800

    fixed 405 error

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,14 @@
-var userName = document.forms['form']['userName'];
+var userName = document.forms['form']['name'];
 var name_error = document.getElementById('name_error');
 
 function validation() {
     if (userName.value !== "Krystel") {
         name_error.textContent = "Invalid name. Lagay mo yung name mo hahahah.";
         name_error.style.color = 'red';
-        return false; // Prevent form submission
+        return false; 
     } else {
         name_error.textContent = ''; 
+        window.location.href = "/docs/inv.html";
         return true; 
     }
 }
