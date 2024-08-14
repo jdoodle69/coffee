3d7b8b65fc645229c953db1e2b119583c7124490
JD <izukanigawa@gmail.com>
2024-08-14 21:12:39 +0800

    fixed script

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
-var name = document.forms['form']['name'];
+var userName = document.forms['form']['userName'];
 var name_error = document.getElementById('name_error');
 
 function validation() {
-    if (name.value !== 'Krystel') {
+    if (userName.value !== 'Krystel') {
         name_error.textContent = 'Invalid name. Lagay mo yung name mo hahahah.';
         name_error.style.color = 'red';
         return false; // Prevent form submission
