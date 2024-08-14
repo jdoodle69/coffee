075682360efbfe8731ebbce2036921bd6645d6cd
JD <izukanigawa@gmail.com>
2024-08-14 22:25:56 +0800

    updated js

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,18 @@
 var userName = document.forms['form']['name'];
 var name_error = document.getElementById('name_error');
 
-function validation() {
+function validation(event) {
+    event.preventDefault(); 
+
     if (userName.value !== "Krystel") {
         name_error.textContent = "Invalid name. Lagay mo yung name mo hahahah.";
         name_error.style.color = 'red';
         return false; 
     } else {
         name_error.textContent = ''; 
-        window.location.href = "/docs/inv.html";
+        window.location.href = "/docs/inv.html"; 
         return true; 
     }
 }
+
+document.forms['form'].onsubmit = validation;
